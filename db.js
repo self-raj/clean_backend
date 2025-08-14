1ad3d312cdafe9bf81017144842f4d4b7d231d31
self-raj <rajbhadurs809@gmail.com>
2025-08-15 02:52:40 +0530

    second  commit

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
