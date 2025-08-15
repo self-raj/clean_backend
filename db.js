df7086c88e4a760420ac8b4150519d3cce88fdba
self-raj <rajbhadurs809@gmail.com>
2025-08-15 14:31:27 +0530

    third  commit

--- db.js
+++ db.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URLgit);
     console.log('✅ MongoDB connected successfully');
   } catch (error) {
     console.error('❌ MongoDB connection failed:', error.message);
